ec58696d4ac7166a848c4bcf8f4a61daef09bead
liuhaidong <Haidong.liu@outlook.com>
2020-02-12 20:21:46 +0800

    add eureka client,feign

---
+++ eureka-server/src/test/java/com/arkdex/springinaction/eurekaclient/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arkdex.springinaction.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

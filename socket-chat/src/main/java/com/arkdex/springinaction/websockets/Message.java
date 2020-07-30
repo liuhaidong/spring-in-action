f80a15fc8e67223232ceecc087db0a1a8e44479a
liuhaidong <Haidong.liu@outlook.com>
2020-07-30 21:27:12 +0800

    add socket

---
+++ socket-chat/src/main/java/com/arkdex/springinaction/websockets/Message.java
@@ -0,0 +1,15 @@
+package com.arkdex.springinaction.websockets;
+
+public class Message {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
